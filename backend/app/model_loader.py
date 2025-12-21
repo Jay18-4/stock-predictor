a862b1de44d775838318f70d7ac43ca38fc2eac9
Chimdalu Ifediba <chimdaluifediba@gmail.com>
2025-12-22 00:57:27 +0100

    Create model_loader.py

---
+++ backend/app/model_loader.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import joblib
+from storage import download_file
+
+MODEL_KEY = "models/v3.2(sentiment)_lstm_stock_pediction.keras"
+LOCAL_MODEL_PATH = "/tmp/v3.2(sentiment)_lstm_stock_pediction.keras"
+SCALER_KEY = 'scaler_v3.1.save'
+LOCAL_SCALER_PATH = "/tmp/scaler_v3.1.save"
+
+_model = None
+_scaler = None
+
+def get_model():
+    global _model
+
+    if _model is None:
+        download_file(MODEL_KEY, LOCAL_MODEL_PATH)
+
+        _model = tf.keras.models.load_model(
+            LOCAL_MODEL_PATH,
+            compile=False  # IMPORTANT: avoids unnecessary memory usage
+        )
+
+    return _model
+
+def get_scaler():
+    global _scaler
+
+    if _scaler is None:
+        download_file(SCALER_KEY, LOCAL_MODEL_PATH)
+
+        _scaler = joblib.load(LOCAL_SCALER_PATH)
+
+    return _scaler
